59d35389d04ba210f840ff54a315bd6e565cbda6
d1618033 <d1618033@gmail.com>
2015-04-22 19:05:59 +0300

    add message, contacts and contact request models

--- private_chat/models.py
+++ private_chat/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name="sender")
+    receiver = models.ForeignKey(User, related_name="receiver")
+    message = models.TextField()
+
+
+class Contacts(models.Model):
+    user1 = models.ForeignKey(User, related_name="user1")
+    user2 = models.ForeignKey(User, related_name="user2")
+
+
+class ContactRequests(models.Model):
+    user = models.ForeignKey(User)
+    requester = models.ForeignKey(User, related_name="requester")
+    message = models.TextField(null=True, blank=True)
+
