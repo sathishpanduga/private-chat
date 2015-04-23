6c3ac9db2920f1c05065c08c5cd9e31ab89f4ca2
d1618033 <d1618033@gmail.com>
2015-04-23 17:55:38 +0300

    add profile view

--- private_chat/views.py
+++ private_chat/views.py
@@ -1,5 +1,12 @@
 from django.views.generic import TemplateView
+from django.core.urlresolvers import reverse_lazy
+from braces.views import LoginRequiredMixin
 
 
 class Index(TemplateView):
     template_name = "private_chat/index.html"
+
+
+class Profile(LoginRequiredMixin, TemplateView):
+    template_name = "private_chat/profile.html"
+    login_url = reverse_lazy('private-chat:login')
