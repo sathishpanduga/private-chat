f067e937d9a6b321f6b94c8017701a3691ea3c0c
d1618033 <d1618033@gmail.com>
2015-04-23 18:20:20 +0300

    minor name changes

--- private_chat/models.py
+++ private_chat/models.py
@@ -3,18 +3,18 @@ from django.contrib.auth.models import User
 
 
 class Message(models.Model):
-    sender = models.ForeignKey(User, related_name="sender")
-    receiver = models.ForeignKey(User, related_name="receiver")
+    sender = models.ForeignKey(User, related_name="message_sender")
+    receiver = models.ForeignKey(User, related_name="message_receiver")
     message = models.TextField()
 
 
-class Contacts(models.Model):
-    user1 = models.ForeignKey(User, related_name="user1")
-    user2 = models.ForeignKey(User, related_name="user2")
+class Contact(models.Model):
+    requester = models.ForeignKey(User, related_name="contact_requested")
+    accepter = models.ForeignKey(User, related_name="contact_accepted")
 
 
-class ContactRequests(models.Model):
+class ContactRequest(models.Model):
     user = models.ForeignKey(User)
-    requester = models.ForeignKey(User, related_name="requester")
+    requester = models.ForeignKey(User, related_name="contact_requester")
     message = models.TextField(null=True, blank=True)
 
