020cde43d6b7edcd31d5fbb83b91a5a688c238f4
d1618033 <d1618033@gmail.com>
2015-04-23 17:55:25 +0300

    add profile and login urls

--- private_chat/urls.py
+++ private_chat/urls.py
@@ -4,4 +4,7 @@ from private_chat import views
 
 urlpatterns = patterns('',
     url('^$', views.Index.as_view(), name='index'),
+    url('^profile/$', views.Profile.as_view(), name='profile'),
+    url('^login/$', 'django.contrib.auth.views.login', kwargs={'template_name': 'private_chat/login.html'},
+        name='login'),
 )
