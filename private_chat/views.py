3806d2ae6601aee1915916b8ebc5a6363c9a851e
d1618033 <d1618033@gmail.com>
2015-04-24 19:22:42 +0300

    add chat view

--- private_chat/views.py
+++ private_chat/views.py
@@ -1,17 +1,31 @@
 from django.views.generic import TemplateView
-from django.core.urlresolvers import reverse_lazy
 from braces.views import LoginRequiredMixin
+
+from private_chat.models import ChatUser
 
 
 class Index(TemplateView):
     template_name = "private_chat/index.html"
 
 
-class Profile(LoginRequiredMixin, TemplateView):
-    template_name = "private_chat/profile.html"
-    login_url = reverse_lazy('private-chat:login')
-
+class ChatTemplate:
     def get_context_data(self, **kwargs):
-        context = super(Profile, self).get_context_data(**kwargs)
+        context = super(ChatTemplate, self).get_context_data(**kwargs)
         context['chatuser'] = self.request.user.chatuser
         return context
+
+
+class Profile(LoginRequiredMixin, ChatTemplate, TemplateView):
+    template_name = "private_chat/profile.html"
+
+
+class Chat(LoginRequiredMixin, ChatTemplate, TemplateView):
+    template_name = "private_chat/chat.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(Chat, self).get_context_data(**kwargs)
+        otheruser = ChatUser.objects.get(username=self.kwargs['username'])
+        context['other_chatuser'] = otheruser
+        context['chat_messages'] = self.request.user.chatuser.messages(otheruser)
+        return context
+
