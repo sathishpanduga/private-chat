111f6465c312be54da27a59d2797f9bfff6154a5
d1618033 <d1618033@gmail.com>
2015-04-24 18:55:19 +0300

    make custom user model to simplify getting contacts

--- private_chat/models.py
+++ private_chat/models.py
@@ -1,20 +1,32 @@
+from itertools import chain
+
 from django.db import models
 from django.contrib.auth.models import User
 
 
+class ChatUser(User):
+    user = models.OneToOneField(User)
+
+    def contacts(self):
+        return chain(
+            map(lambda x: x.requester, self.contact_accepted.all()),
+            map(lambda x: x.accepter, self.contact_requested.all())
+        )
+
+
 class Message(models.Model):
-    sender = models.ForeignKey(User, related_name="message_sender")
-    receiver = models.ForeignKey(User, related_name="message_receiver")
+    sender = models.ForeignKey(ChatUser, related_name="message_sender")
+    receiver = models.ForeignKey(ChatUser, related_name="message_receiver")
     message = models.TextField()
 
 
 class Contact(models.Model):
-    requester = models.ForeignKey(User, related_name="contact_requested")
-    accepter = models.ForeignKey(User, related_name="contact_accepted")
+    requester = models.ForeignKey(ChatUser, related_name="contact_requested")
+    accepter = models.ForeignKey(ChatUser, related_name="contact_accepted")
 
 
 class ContactRequest(models.Model):
-    user = models.ForeignKey(User)
-    requester = models.ForeignKey(User, related_name="contact_requester")
+    user = models.ForeignKey(ChatUser)
+    requester = models.ForeignKey(ChatUser, related_name="contact_requester")
     message = models.TextField(null=True, blank=True)
 
