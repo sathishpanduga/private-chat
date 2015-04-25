28e551310a0d93c0fc212326ac4461c1df637f80
d1618033 <d1618033@gmail.com>
2015-04-25 15:45:34 +0300

    use add_message from models.py

--- tornado_app/websockets.py
+++ tornado_app/websockets.py
@@ -1,38 +1,15 @@
 import json
-import sqlite3
-import datetime
 
 import tornado.websocket
 
-
-conn = sqlite3.connect("db.sqlite3")
-
-
-def get_user_id(username):
-    return conn.execute("SELECT id FROM auth_user WHERE username = :username",
-                        {"username": username}).fetchone()[0]
-
-
-def add_message(sender, receiver, message):
-    created = datetime.datetime.now()
-    conn.execute(
-        "INSERT INTO private_chat_message (sender_id, receiver_id, message, created)"
-        " VALUES (:sender_id, :receiver_id, :message, :created)",
-        {
-            "sender_id": get_user_id(sender),
-            "receiver_id": get_user_id(receiver),
-            "message": message,
-            "created": created,
-        }
-    )
-    conn.commit()
-    return created
+from tornado_app.models import add_message
 
 
 class MessageHandler(tornado.websocket.WebSocketHandler):
     clients = []
 
     def open(self):
+        print(type(self))
         self.clients.append(self)
 
     def on_message(self, message):
