6ee70c26249afb71e5eb14a0164b9de85a0ec2e3
d1618033 <d1618033@gmail.com>
2015-04-25 20:36:30 +0300

    create add_contact function

--- tornado_app/models.py
+++ tornado_app/models.py
@@ -24,3 +24,27 @@ def add_message(sender, receiver, message):
     )
     conn.commit()
     return created
+
+
+def add_contact(accepter, requester):
+    accepter_id = get_user_id(accepter)
+    requester_id = get_user_id(requester)
+    print("{0}({1}) accepted request from {2}({3})".format(accepter, accepter_id, requester, requester_id))
+    conn.execute(
+        "INSERT INTO private_chat_contact (accepter_id, requester_id)"
+        " VALUES (:accepter_id, :requester_id)",
+        {
+            "accepter_id": accepter_id,
+            "requester_id": requester_id,
+        }
+    )
+    conn.execute(
+        "DELETE FROM private_chat_contactrequest"
+        " WHERE accepter_id = :accepter_id"
+        " AND requester_id = :requester_id",
+        {
+            "accepter_id": accepter_id,
+            "requester_id": requester_id,
+        }
+    )
+    conn.commit()
