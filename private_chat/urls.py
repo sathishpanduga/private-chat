3a343ad23d74f76d1da17be134cce5601b4cb5bc
d1618033 <d1618033@gmail.com>
2015-04-25 20:24:18 +0300

    redirect index to profile

--- private_chat/urls.py
+++ private_chat/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, url
+from django.views.generic import RedirectView
+from django.core.urlresolvers import reverse_lazy
 
 from private_chat import views
 
 
 urlpatterns = patterns('',
-    url('^$', views.Index.as_view(), name='index'),
+    url('^$', RedirectView.as_view(url=reverse_lazy('private-chat:profile')), name='index'),
     url('^profile/$', views.Profile.as_view(), name='profile'),
     url('^chats/(?P<username>.*?)/$', views.Chat.as_view(), name='chat'),
     url('^login/$', 'django.contrib.auth.views.login', kwargs={'template_name': 'private_chat/login.html'},
