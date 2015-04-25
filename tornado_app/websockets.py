6e7100ea972127f780f480f6fa2350321e02dc17
d1618033 <d1618033@gmail.com>
2015-04-25 20:26:53 +0300

    separate broadcasting logic

--- tornado_app/websockets.py
+++ tornado_app/websockets.py
@@ -5,17 +5,23 @@ import tornado.websocket
 from tornado_app.models import add_message
 
 
-class MessageHandler(tornado.websocket.WebSocketHandler):
+class Broadcaster(tornado.websocket.WebSocketHandler):
     clients = []
 
     def open(self):
         print(type(self))
         self.clients.append(self)
 
+    def broadcast(self, message):
+        for client in self.clients:
+            client.write_message(message)
+
+
+class MessageHandler(Broadcaster):
     def on_message(self, message):
+        print("got: "+message)
         message = json.loads(message)
         created = add_message(**message)
-        for client in self.clients:
-            client.write_message("{0} [{1}]: {2}".format(
-                created.strftime('%H:%M:%S'), message["sender"], message["message"]
-            ))
+        self.broadcast("{0} [{1}]: {2}".format(
+            created.strftime('%H:%M:%S'), message["sender"], message["message"]
+        ))
