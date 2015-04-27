140e9ce04f40355f9b3f5a0d35ff11d62e281c93
d1618033 <d1618033@gmail.com>
2015-04-27 21:27:49 +0300

    add contact requests url

--- tornado_app/urls.py
+++ tornado_app/urls.py
@@ -3,4 +3,5 @@ from tornado_app import websockets
 urlpatterns = [
     (r'/messages', websockets.MessageHandler),
     (r'/contacts', websockets.ContactsHandler),
+    (r'/contactrequests', websockets.ContactRequestsHandler),
 ]
